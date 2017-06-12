ed96c1eb550ff6e754aea4ed715a4ca61339b356
joung-young choi <love5757@me.com>
2017-06-12 13:03:24 +0900

    modify

--- src/main/java/kr/yerina/wmp/autonomousRegistration/AutonomousRegistrationApplication.java
+++ src/main/java/kr/yerina/wmp/autonomousRegistration/AutonomousRegistrationApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class AutonomousRegistrationApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AutonomousRegistrationApplication.class, args);
 	}
