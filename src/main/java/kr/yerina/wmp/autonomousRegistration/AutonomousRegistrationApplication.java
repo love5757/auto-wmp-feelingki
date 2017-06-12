f7ece833ab78aaec030d05bdb85521acc7f75ccf
joung-young choi <love5757@me.com>
2017-06-12 21:24:51 +0900

    modify

--- src/main/java/kr/yerina/wmp/autonomousRegistration/AutonomousRegistrationApplication.java
+++ src/main/java/kr/yerina/wmp/autonomousRegistration/AutonomousRegistrationApplication.java
@@ -1,10 +1,15 @@
 package kr.yerina.wmp.autonomousRegistration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.WebApplicationInitializer;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import java.util.TimeZone;
 
 @SpringBootApplication
